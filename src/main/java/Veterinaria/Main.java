d7f63f2bc7de0d3697967fda7e46626edd543aa2
unknown <vanesa.carballido@correo.ucu.edu.uy>
2025-06-30 16:24:11 -0300

    ojala funcione

--- src/main/java/Veterinaria/Main.java
+++ src/main/java/Veterinaria/Main.java
@@ -7,18 +7,24 @@ import Veterinaria.clases.Veterinario;
 public class Main {
     public static void main(String[] args) {
         SalaDeEspera sala = new SalaDeEspera(6);
+
+        // Crear veterinarios
         Veterinario vet1 = new Veterinario("Dra. Vanesa", sala);
         Veterinario vet2 = new Veterinario("Dra. Isabela", sala);
-        vet1.start();
-        vet2.start();
+
+        // Iniciar la atención de los veterinarios usando semáforo
+        new Thread(() -> vet1.atender()).start();
+        new Thread(() -> vet2.atender()).start();
 
         synchronized (System.out) {
             System.out.println("VETERINARIA ABIERTA");
         }
+
+        // Crear clientes aleatorios
         for (int i = 1; i <= 10; i++) {
             new Thread(() -> {
                 try {
-                    Thread.sleep((int) (Math.random() * 5000));
+                    Thread.sleep((int) (Math.random() * 5000)); // Tiempo aleatorio de espera
                     Cliente c = new Cliente(nombreRandom(), razaRandom());
                     sala.llegaCliente(c);
                 } catch (InterruptedException e) {
@@ -28,15 +34,16 @@ public class Main {
         }
     }
 
+    // Métodos para generar nombres y razas aleatorios
     private static String razaRandom() {
         String[] razas = {"Pastor Alemán", "Golden Retriever", "Caniche", "Doberman", "Salchicha",
-                "Gran Danés", "Yorkshire", "Siamés", "Corgi", "Labrador", "Persa", "Carey", "cruza",};
+                "Gran Danés", "Yorkshire", "Siamés", "Corgi", "Labrador", "Persa", "Carey", "cruza"};
         return razas[(int) (Math.random() * razas.length)];
     }
 
     private static String nombreRandom() {
         String[] nombres = {"Juan", "Felipe", "Ana", "Florencia", "Dolores",
-        "Andrea", "Marcos", "Agustín", "Constanza"};
+                "Andrea", "Marcos", "Agustín", "Constanza"};
         return nombres[(int) (Math.random() * nombres.length)];
     }
 }
--- src/main/java/Veterinaria/clases/Veterinario.java
+++ src/main/java/Veterinaria/clases/Veterinario.java
@@ -1,9 +1,14 @@
 package Veterinaria.clases;
 
-public class Veterinario extends Thread {
+import java.util.concurrent.Semaphore;
+
+public class Veterinario {
     private final String nombre;
     private final SalaDeEspera sala;
     private int siCierra;
+
+    // Semáforo para controlar los veterinarios disponibles (2 veterinarios)
+    private static final Semaphore veterinariosDisponibles = new Semaphore(2);
 
     public Veterinario(String nombre, SalaDeEspera sala) {
         this.nombre = nombre;
@@ -14,19 +19,27 @@ public class Veterinario extends Thread {
         return this.nombre;
     }
 
-    @Override
-    public void run() {
+    public void atender() {
         while (true) {
             try {
-                System.out.println(this.nombre + " esperando cliente...");
-               boolean hayCliente = sala.intentarEsperarCliente();
+                // Intentamos adquirir un permiso del semáforo
+                veterinariosDisponibles.acquire();
+                System.out.println(this.nombre + " está esperando cliente...");
+
+                // Verificamos si hay clientes para atender
+                boolean hayCliente = sala.intentarEsperarCliente();
                 if (hayCliente) {
                     sala.atenderCliente(this.nombre);
                 } else {
                     this.ordenar();
                 }
-            }
-            catch (InterruptedException e) {
+
+                // Liberamos el semáforo para que otro veterinario pueda atender
+                veterinariosDisponibles.release();
+
+                // Dormimos un poco para simular el trabajo
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
@@ -34,7 +47,7 @@ public class Veterinario extends Thread {
 
     public void ordenar() {
         synchronized (System.out) {
-            System.out.println(this.nombre+" esta ordenando...");
+            System.out.println(this.nombre + " está ordenando...");
         }
         siCierra++;
         if (siCierra == 6) {
@@ -45,10 +58,8 @@ public class Veterinario extends Thread {
         }
         try {
             Thread.sleep(1500);
-        }
-        catch (InterruptedException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
-
 }
